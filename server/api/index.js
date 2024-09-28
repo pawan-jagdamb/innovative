44657d220d19ca0ae46867022022a757f4052461
pawan-jagdamb <pawankumar9534078@gmail.com>
2024-09-28 10:00:46 +0530

    Database Is Created

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import mongoose from 'mongoose'
+import 'dotenv/config'
+const PORT= process.env.PORT;
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    console.log("connection to database is successfull");
+})
+.catch((error)=>{  
+    console.log("Error in Connecting database");
+})
 
 const app=express();
-app.listen(3000,()=>{
-    console.log('Server is running on port!!!')
-}) 
+app.listen(PORT,()=>{
+    console.log('Server is running on port!!! ',PORT)
+})    
