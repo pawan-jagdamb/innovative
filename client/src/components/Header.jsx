299caf90757bed579d2450999495a51f9824cf8a
pawan-jagdamb <pawankumar9534078@gmail.com>
2025-03-26 22:00:21 +0530

    update header search form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,53 +1,86 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
-import { Link } from 'react-router-dom';
-import {useSelector} from 'react-redux'
-
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-  const {currentUser}= useSelector(state=>state.user);
-  console.log("Current user->",currentUser);
-//   console.log("Current user.user->",currentUser.user.avatar);
+  const { currentUser } = useSelector((state) => state.user);
+  console.log("Current user->", currentUser);
+  const navigate= useNavigate();
+  const [searchTerm,setSearchTerm]= useState('')
+  //   console.log("Current user.user->",currentUser.user.avatar);
+  const handleSubmit=(e)=>{
+    e.preventDefault();
 
+    const urlParams= new URLSearchParams(window.location.search);
+
+    urlParams.set('searchTerm',searchTerm);
+    
+    const searchQuery= urlParams.toString();
+    navigate(`/search?${searchQuery}`)
+    
+    
+
+  }
+
+  useEffect(()=>{
+    const urlParams= new URLSearchParams(window.location.search);
+
+    const searchTermFromUrl= urlParams.get('searchTerm')
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl)
+    }
+
+  },[location.search])
   return (
-    <header className='text-xl bg-richblack-800 shadow-md'>
-    <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <Link to='/'>
-                <span className='text-richblack-25'>Colligiate </span>
-                <span className=' text-richblack-100'>Mart</span>
-            </Link>
-           
+    <header className="text-xl bg-richblack-800 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <Link to="/">
+            <span className="text-richblack-25">Colligiate </span>
+            <span className=" text-richblack-100">Mart</span>
+          </Link>
         </h1>
 
-        <form className='bg-slate-100 rounded-lg p-3 flex items-center'>
-            <input type=" text" placeholder='Search Products...' 
-                className='bg-transparent outline-none w-24 sm:w-64'
+        <form onSubmit={handleSubmit} className="bg-slate-100 rounded-lg p-3 flex items-center">
+          <input
+            type=" text"
+            placeholder="Search Products..."
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target.value)}
 
-            />
-            <IoSearch className='text-slate-600'/>
+            className="bg-transparent outline-none w-24 sm:w-64"
+          />
+          <button >
+
+          <IoSearch className="text-slate-600" />
+          </button>
         </form>
-        <ul className='flex gap-4'>
-        <Link to='/'>
-           <li className='hidden sm:inline text-richblack-100 hover:underline '>Home</li>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-richblack-100 hover:underline ">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-richblack-100 hover:underline">
+              About
+            </li>
+          </Link>
 
-        </Link>
-        <Link to='/about'>
-        <li className='hidden sm:inline text-richblack-100 hover:underline'>About</li>
-
-
-        </Link>
-        
-        <Link to='/profile'>
-        {currentUser?(<img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='Profile'/>) : (<li className=' text-richblack-100 hover:underline'>Sign in</li>)
-        }
-
-        </Link>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="Profile"
+              />
+            ) : (
+              <li className=" text-richblack-100 hover:underline">Sign in</li>
+            )}
+          </Link>
         </ul>
-
-    </div>
-       
-
+      </div>
     </header>
-  )
+  );
 }
